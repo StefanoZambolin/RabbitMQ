7cd2d2edde97234f2caf3f4d53cc8452def713b4
StefanoZambolin <70204526+StefanoZambolin@users.noreply.github.com>
2024-03-09 22:51:11 +0100

    Add project files.

---
+++ Send 2/Send2.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+const string message = "Hello World #2!";
+var body = Encoding.UTF8.GetBytes(message);
+
+Timer timer = new Timer(sendIt, null, TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(5));
+
+void sendIt(object? state)
+{
+    channel.BasicPublish(exchange: string.Empty,
+                         routingKey: "hello",
+                         basicProperties: null,
+                         body: body);
+    Console.WriteLine($" [x] Sent {message}");
+
+
+}
+
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
---
+++ Send/Send.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+
+
+
+var timer = new Timer(sendIt, null, TimeSpan.Zero, TimeSpan.FromSeconds(2));
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+void sendIt(object? state)
+{
+    const string message = "Hello World #3!";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: string.Empty,
+                         routingKey: "hello",
+                         basicProperties: null,
+                         body: body);
+    Console.WriteLine($" [x] Sent {message}");
+
+}
+
